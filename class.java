c1af3df40ddd3dc665e4b1a20d5dff63f3c2804e
su2700 <45990539+su2700@users.noreply.github.com>
2020-10-10 20:51:58 +0300

    Add files via upload

---
+++ class.java
@@ -0,0 +1,150 @@
+import java.util.Scanner;
+
+class MyClass
+{
+    public static void
+    main(String[ ] args)
+    {
+        class player {
+            int playerNum;
+            String Fname;
+            String Lname;
+            int position;
+            int age;
+            double height;
+            double armSpan;
+            double salary;
+
+            public player(
+                    int playerNum,
+                    String Fname,
+                    String Lname,
+                    int position;
+                    int age;
+                    double height;
+                    double armSpan;
+                    double salary;)
+            {
+                this.playerNum = playerNum;
+                this.Fname = Fname;
+                this.Lname = Lname;
+                this.position = position;
+                this.age = age;
+                this.height=height;
+                this.armSpan=armSpan;
+                this.salary = salary;
+            }
+
+
+        }
+        void printPlayer(){
+        System.out.println(
+                "player's Number:" + playNum;
+                   "Name: " + Fname+ Lname;
+                   "Position: " + position;
+                   "Age:"+ age;
+                   "Height:" + height;
+                   "Arm Span:" + armSpan;
+                   "Salary: " + salary;
+        );}
+        class coach {
+            int coachNum;
+            String Fname;
+            String Lname;
+            int level;
+            int age;
+            boolean male;
+            double salary;
+
+            public coach(
+                    int coachNum,
+                    String Fname,
+                    String Lname,
+                    int level;
+                            int age;
+                            boolean male;
+                            double salary;)
+            {
+                this.coachNum = coachNum;
+                this.Fname = Fname;
+                this.Lname = Lname;
+                this.level = level;
+                this.age = age;
+                this.male=male;
+                this.salary = salary;
+            }
+        }
+        class fan {
+            int fanNum;
+            String Fname;
+            String Lname;
+            int age;
+            boolean male;
+            int ticketType;
+
+            public fan(
+                    int fanNum;
+                    String Fname;
+                    String Lname;
+                    int age;
+                    boolean male;
+                    int ticketType;)
+            {
+                this.fanNum = fanNum;
+                this.Fname = Fname;
+                this.Lname = Lname;
+                this.age = age;
+                this.male=male;
+                this.ticketType = ticketType;
+            }
+        }
+        class manager{
+            int staffNum;
+            String Fname;
+            String Lname;
+            int position;
+            int age;
+            double salary;
+
+            public manager(
+                    int staffNum,
+                    String Fname,
+                    String Lname,
+                    int position;
+                            int age;
+                            double salary;)
+            {
+                this.staffNum = staffNum;
+                this.Fname = Fname;
+                this.Lname = Lname;
+                this.position = position;
+                this.age = age;
+                this.salary = salary;
+            }
+        }
+        class supportStaff {
+            int staffNum;
+            String Fname;
+            String Lname;
+            string position;
+            int age;
+            double salary;
+
+            public supportStaff(
+                    int staffNum,
+                    String Fname,
+                    String Lname,
+                    string position;
+                            int age;
+                            double salary;)
+            {
+                this.staffNum = staffNum;
+                this.Fname = Fname;
+                this.Lname = Lname;
+                this.position = position;
+                this.age = age;
+                this.salary = salary;
+            }
+        }
+    }
+}
